98cb29a8adbcbd63d687df62c1352b38e9db3ec7
yycho0108 <jchocholate@gmail.com>
2018-03-01 20:22:56 -0500

    removing deprecated code

--- main_rnn.py
+++ main_rnn.py
@@ -53,7 +53,7 @@ N_ANNEAL = 400000 #How many steps of training to reduce startE to endE.
 EPS_DECAY = EPS_MIN ** (1.0/N_ANNEAL)
 #EPS_DECAY = 0.9999
 
-N_PRE = 50000 #Number of steps, pre-train
+N_PRE = 5000 #Number of steps, pre-train
 N_MEM = 100000
 
 def proc(x):
@@ -101,6 +101,7 @@ class DRQNMain(object):
         writer = tf.summary.FileWriter(os.path.join(self._dirs['run_log_root'], 'train'), graph)
         tf.summary.scalar('loss', drqn_a['loss'])
         summary = tf.summary.merge_all()
+        saver = tf.train.Saver()
         
         self._drqn_a = drqn_a
         self._drqn_c = drqn_c
@@ -110,6 +111,7 @@ class DRQNMain(object):
         self._writer = writer
         self._summary = summary
         self._tf_step = tf_step
+        self._saver = saver
 
     def act(self, s, c, h):
         env  = self._env
@@ -265,138 +267,16 @@ class DRQNMain(object):
         sig.stop() # i.e. stop handling signals
         return rewards
 
+    def save(self, path='/tmp/model.ckpt'):
+        save_path = self._saver.save(self._sess, path)
+        print("Model saved in file: %s" % save_path)
+
+    def load(self, path='/tmp/model.ckpt'):
+        ## TODO : actually not ignore path
+        self._saver.restore(sess, path)
+
     def run(self, *args, **kwargs):
         return self._sess.run(*args, **kwargs)
-
-def train(
-        net, target_net,
-        memory, episodes,
-        copy_ops, train_ops,
-        tf_step
-        ):
-    eps = EPS_INIT
-    step = 0
-    rewards = []
-    max_step = 999 # TODO : configure this
-
-    writer = tf.summary.FileWriter(os.path.join(dirs['run_log_root'], 'train'), sess.graph)
-    tf.summary.scalar('loss', net['loss'])
-    summary = tf.summary.merge_all()
-
-    c0 = np.zeros([1, N_H])
-    h0 = np.zeros([1, N_H])
-
-    def train_once(eps, step):
-        s0 = proc(env.reset())
-
-        # initialize episode
-        net_reward = 0
-        d = False
-        c = c0.copy()
-        h = h0.copy()
-        entry = []
-
-        for j in range(max_step):
-            s0_1 = np.expand_dims(s0, 0)
-
-            # act()
-            if (np.random.random() < eps) or (step < N_PRE):
-                c, h = run(sess, net._tensors, ['x_in', 'c_in', 'h_in', 'n_b', 'n_t'],
-                        [s0_1, c, h, 1, 1], ['c_out', 'h_out'])
-                a = env.action_space.sample()
-            else:
-                a, c, h = run(sess, net._tensors, ['x_in', 'c_in', 'h_in', 'n_b', 'n_t'],
-                        [s0_1, c, h, 1, 1], ['a_y', 'c_out', 'h_out'])
-                a = a[0]
-
-            s1,r,d,_ = env.step(a)
-            s1 = proc(s1)
-
-            entry.append(s0 + [a,r] + s1 + [d]) #[0:2, 2, 3, 4:6, 6]
-
-            if step > N_PRE:
-                if eps > EPS_MIN:
-                    eps = max(EPS_MIN, eps * EPS_DECAY)
-                if (step % U_FREQ) == 0:
-
-                    input_batch = memory.sample(N_BATCH, N_TRACE)
-
-                    x0_in, a_in, r_in, x1_in, d_in = np.split(input_batch, np.cumsum([N_X,1,1,N_X]), axis=-1)
-
-                    x0_in = np.reshape(x0_in, [-1, N_X])
-                    a_in = np.reshape(a_in, [-1])
-                    r_in = np.reshape(r_in, [-1])
-                    x1_in = np.reshape(x1_in, [-1, N_X])
-                    d_in = np.reshape(d_in, [-1])
-
-                    c_in = np.zeros([N_BATCH, N_H])
-                    h_in = np.zeros([N_BATCH, N_H])
-
-                    a, q = sess.run([net['a_y'], target_net['q_y']], feed_dict={
-                        net['x_in'] : x1_in,
-                        net['c_in'] : c_in,
-                        net['h_in'] : h_in,
-                        net['n_b'] : N_BATCH,
-                        net['n_t'] : N_TRACE,
-                        target_net['x_in'] : x1_in,
-                        target_net['c_in'] : c_in,
-                        target_net['h_in'] : h_in,
-                        target_net['n_b'] : N_BATCH,
-                        target_net['n_t'] : N_TRACE
-                        })
-
-                    q = q[range(N_BATCH*N_TRACE), a] # "real" q values
-                    q_t = r_in + GAMMA * q * (1 - d_in) # discounted target q
-
-                    # update ... 
-                    s, _ = sess.run([summary, train_ops],
-                            feed_dict = {
-                                net['x_in'] : x0_in,
-                                net['c_in'] : c_in,
-                                net['h_in'] : h_in,
-                                net['n_b'] : N_BATCH,
-                                net['n_t'] : N_TRACE,
-                                net['q_t'] : q_t,
-                                net['a_t'] : a_in,
-                                tf_step : (step - N_PRE)
-                                })
-                    writer.add_summary(s, step)
-
-                    sess.run(copy_ops) # update ...
-
-            net_reward += r
-
-            s0 = s1
-            step += 1
-
-            if d:
-                break
-        memory.add(np.asarray(entry))
-        return net_reward, eps, step
-
-    r_mean = 0
-    i = 0
-    sig = StopRequest()
-    sig.start()
-
-    while not sig._stop:
-        net_reward, eps, step = train_once(eps, step)
-
-        writer.add_summary(tf.Summary(value=[tf.Summary.Value(
-            tag='net_reward',
-            simple_value = net_reward
-            )]), step)
-        
-        rewards.append(net_reward)
-
-        if i % 100 == 0 and i > 0:
-            r_mean = np.mean(rewards[-100:])
-            r_max = np.max(rewards[-100:])
-            print "[%d:%d] r(mean,max) (%.2f,%.2f) | Eps: %f" % (i, step, r_mean, r_max, eps)
-        i += 1
-    sig.stop()
-
-    return rewards
 
 def test(net, episodes):
     rewards = []
@@ -437,50 +317,22 @@ def main():
             reward_threshold=475.0,
             )
     env = gym.make('CartPole-v0500')
-
     app = DRQNMain(env)
-    ## setup ... 
-    #drqn_a = DRQN([N_X], N_A, N_TRACE, scope='actor')
-    #drqn_c = DRQN([N_X], N_A, N_TRACE, scope='critic')
-    #memory = TraceMemory(size=N_MEM)
 
-    ## critic-update ...
-    #va = drqn_a.get_trainable_variables()
-    #vc = drqn_c.get_trainable_variables()
-
-    #copy_ops = [c.assign(a.value()*TAU + c.value()*(1.0-TAU)) for (a,c) in zip(va,vc)]
-    ##copy_ops = [c.assign(a.value()) for (a,c) in zip(va,vc)]
-    #copy_ops = tf.group(copy_ops)
-
-    ## train ...
-    #tf_step = tf.Variable(0, trainable=False)
-    #lr = tf.maximum(tf.train.exponential_decay(LR_MAX, tf_step, LR_DECAY_STEPS, LR_MIN/LR_MAX), LR_MIN)
-    #tf.summary.scalar('learning_rate', lr)
-    #trainer = tf.train.AdamOptimizer(learning_rate=lr)
-    #train_ops = trainer.minimize(
-    #        drqn_a._tensors['loss'],
-    #        var_list = va
-    #        )
-
-    ## initialize...
-    #sess.run(tf.global_variables_initializer())
-    #sess.run(copy_ops)
-
-    saver = tf.train.Saver()
     if len(sys.argv) > 1 and sys.argv[1].lower() == 'load':
         # load
-        saver.restore(sess, '/tmp/model.ckpt')
+        app.load('/tmp/model.ckpt')
         print '[loaded]'
     else:
         # train
         #train_rewards = train(drqn_a, drqn_c, memory, N_EPOCH, copy_ops, train_ops, tf_step)
         train_rewards = app.train(N_EPOCH)
         np.savetxt('train.csv', train_rewards, delimiter=',', fmt='%f')
-        save_path = saver.save(sess, '/tmp/model.ckpt')
+        save_path = app.save('/tmp/model.ckpt')
         print("Model saved in file: %s" % save_path) 
 
-    test_rewards = test(drqn_a, N_TEST)
-    np.savetxt('test.csv', test_rewards, delimiter=',', fmt='%f')
+    #test_rewards = test(drqn_a, N_TEST)
+    #np.savetxt('test.csv', test_rewards, delimiter=',', fmt='%f')
 
 if __name__ == "__main__":
     main()
